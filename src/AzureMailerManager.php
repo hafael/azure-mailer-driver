648eedebb77cbc9dc017c5bfb6cb5da3505f7524
Juan Diaz <donjuanc@codwelt.com>
2023-11-08 12:46:02 -0500

    Add config http client

--- src/AzureMailerManager.php
+++ src/AzureMailerManager.php
@@ -9,13 +9,14 @@ class AzureMailerManager extends MailManager
     protected function createAzureTransport()
     {
         $config = $this->app['config']->get('mail.mailers.azure', []);
+        $configHttp = isset($config['http']) ? $config['http'] : [];
         
         return new AzureMailerApiTransport(
             $config['endpoint'], 
             $config['access_key'], 
             $config['api_version'], 
             boolval($config['disable_user_tracking']),
-            $this->getHttpClient([]),
+            $this->getHttpClient($configHttp),
         );
     }
 }
